f3afd392faf0909f3633e64404821611a4fd1894
abinashregmi <abinashregmi01234@gmail.com>
2025-01-07 07:42:33 +0545

    ok

--- sidequest/rockpaperscissor.py
+++ sidequest/rockpaperscissor.py
@@ -1,1 +1,27 @@
 import random
+random.randint
+
+lst=['rock','paper', 'scissor']
+alish=random.choice(lst)
+
+# print('Alish Rai won', alish)
+
+anup=random.choice(lst)
+# print('Anup won', anup)
+
+bimal=random.choice(lst)
+# print('Bimal won', bimal)
+
+if alish== "rock" and anup =="scissor" and bimal == "paper":
+    print('anup won')
+    
+elif alish== "rock" and anup == "paper" and bimal == "scissor":
+        print('bimal won')
+elif alish== "rock" and anup == "rock" and bimal == "rock":
+      print("Tie")
+
+elif alish== "paper"and anup=="rock"and bimal=="scissor":
+      print('bimal won')
+
+else:
+      print('bimal won ')
